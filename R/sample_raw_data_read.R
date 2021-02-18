e25a2704ade7b7ab507866493b0ff6a771c6bdf7
Krista Kraskura <kkraskura@ucsb.edu>
2021-02-18 14:20:01 -0500

    Initial commit

---
+++ R/sample_raw_data_read.R
@@ -0,0 +1,17 @@
+
+
+#' Read text file and then start the data analysis sequence
+#'
+#' @param path specifies the path to the internally available data
+#'
+#' @return a \code{tibble}
+#' @export
+#' @importFrom utils read.delim
+#' @examples
+#' txt = system.file("extdata", "raw_txt_data_MMR.txt", package = "respMR")
+#' sample_raw_data_read(txt)
+sample_raw_data_read<-function(path){
+  utils::read.delim(path, skip=19)
+}
+
+# these data will be available for tests, vignettes, etc.
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(respMR)
+
+test_check("respMR")
